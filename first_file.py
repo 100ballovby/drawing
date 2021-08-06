3b5253f534a2119b7830b5ebef4f1b33dcf82ca1
demidraksin <greatraksin@icloud.com>
2021-08-06 15:22:22 +0300

    Добавил документ-строку к функции рисования домика😄

--- first_file.py
+++ first_file.py
@@ -1,4 +1,12 @@
 def draw_house(x, y, width, height):
+    """
+    Функция рисует домик
+    :param x: обязательный, х-координата опорной точки
+    :param y: обязательный, у-координата опорной точки
+    :param width: обязательный, ширина дома
+    :param height: обязательный, высота дома
+    :return: None
+    """
     print('Типа рисую дом', x, y, width, height)
     pass
 
